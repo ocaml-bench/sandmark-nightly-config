96e4e1fa85dbe1e00dfbe8dbdd60304885f82a3f
Jan Midtgaard <mail@janmidtgaard.dk>
2023-09-19 17:28:05 +0200

    Fix exit code in scripts/check-variant-name.sh

--- scripts/check-variant-name.sh
+++ scripts/check-variant-name.sh
@@ -9,6 +9,7 @@ then
     git clone https://github.com/ocaml-bench/sandmark.git "${SANDMARK_DIR}"
 fi
 
+num_err=0
 
 for f in `find config/*.json`; do
     NAMES=$(jq -r 'if type=="array" then .[].name else .name end' "${f}");
@@ -17,6 +18,11 @@ for f in `find config/*.json`; do
         variant_json="${SANDMARK_DIR}/ocaml-versions/${variant}.json"
         if [[ ! -f "${variant_json}" ]]; then
             echo "${variant} is not a valid variant. See https://github.com/ocaml-bench/sandmark/tree/main/ocaml-versions for supported versions!"
+            num_err=$((num_err+1))
         fi
     done
 done
+
+if [[ $num_err -ne 0 ]]; then
+    exit 1
+fi
